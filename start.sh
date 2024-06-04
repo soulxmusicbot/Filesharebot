b5f1541cee20b44269f9ad3e352b22b29039b0bd
soulxmusicbot <rustydragon20@gmail.com>
2024-06-04 17:55:04 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/VJBots/Advance-File-Share-Bot /Advance-File-Share-Bot
+  git clone https://github.com/soulxmusicbot/Filesharebot /Filesharebot
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Advance-File-Share-Bot
 fi
-cd /Advance-File-Share-Bot
+cd /Filesharebot
 pip3 install -U -r requirements.txt
 echo "Bot Started...."
 python3 bot.py
